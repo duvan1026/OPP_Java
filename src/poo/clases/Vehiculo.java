bce929a088ce288eb2a069973b468cafd6663583
duvan1026 <duvancastro1026@gmail.com>
2023-03-17 13:24:05 -0500

    file update

--- src/poo/clases/Vehiculo.java
+++ src/poo/clases/Vehiculo.java
@@ -8,28 +8,41 @@ public class Vehiculo {
     double cc;
     int year;
     boolean sport;
-
+    int speed;
 
 
     // 2. Constructores
-    public Vehiculo(){
+    public Vehiculo() {
 
     }
 
-    public Vehiculo( String fabricante, String modelo, double cc, int year, boolean sport ){
+    public Vehiculo(String fabricante, String modelo, double cc, int year, boolean sport) {
         this.fabricantes = fabricante;
         this.modelo = modelo;
         this.cc = cc;
         this.year = year;
         this.sport = sport;
+        this.speed = 0;
     }
 
-    public Vehiculo( String fabricantes, String modelo ){
+    public Vehiculo(String fabricantes, String modelo) {
         this.fabricantes = fabricantes;
         this.modelo = modelo;
     }
 
+    public Vehiculo(String fabricantes, int year) {
+        this.fabricantes = fabricantes;
+        this.year = year;
+    }
 
     // 3. Metodos (comportamientos)
+    public void acelerar(int quantity) {
+        this.speed += quantity;
+    }
+
+    // getter y setter
+
+
+    //tostring
 
 }
