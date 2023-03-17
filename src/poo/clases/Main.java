88b13d4e36ff931e8459c077a7741964710e3174
duvan1026 <duvancastro1026@gmail.com>
2023-03-17 14:31:24 -0500

    implement class Motor

--- src/poo/clases/Main.java
+++ src/poo/clases/Main.java
@@ -10,7 +10,10 @@ public class Main {
         Vehiculo toyotaPrius = new Vehiculo();
 
         // crear un objeto utilizando el contructor con parámetros
-        Vehiculo forMondeo = new Vehiculo("Ford", "Mondeo", 2.1, 2020, false);
+        Motor motorGTI = new Motor("GTI",190,459.0,6);
+
+        Vehiculo forMondeo = new Vehiculo("Ford", "Mondeo", 2.1, 2020, false,0,motorGTI);
+
         System.out.println(forMondeo.fabricante);
         System.out.println(forMondeo.year);
 
