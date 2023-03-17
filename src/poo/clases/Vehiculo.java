82ec2f1087dd98d1af0d56c83013598099241e06
duvan1026 <duvancastro1026@gmail.com>
2023-03-17 13:17:39 -0500

    implemented constructor of the class in Java

--- src/poo/clases/Vehiculo.java
+++ src/poo/clases/Vehiculo.java
@@ -2,6 +2,34 @@ package poo.clases;
 
 public class Vehiculo {
 
+    // 1. atributos
+    String fabricantes;
+    String modelo;
+    double cc;
+    int year;
+    boolean sport;
 
+
+
+    // 2. Constructores
+    public Vehiculo(){
+
+    }
+
+    public Vehiculo( String fabricante, String modelo, double cc, int year, boolean sport ){
+        this.fabricantes = fabricante;
+        this.modelo = modelo;
+        this.cc = cc;
+        this.year = year;
+        this.sport = sport;
+    }
+
+    public Vehiculo( String fabricantes, String modelo ){
+        this.fabricantes = fabricantes;
+        this.modelo = modelo;
+    }
+
+
+    // 3. Metodos (comportamientos)
 
 }
