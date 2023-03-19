919645a06f69fa29fb408311f013fa293a5d1a33
Duvan Castro <duvancastro1026@gmail.com>
2023-03-19 05:09:09 -0500

    create class

--- src/poo/interfaces/Empleado.java
+++ src/poo/interfaces/Empleado.java
@@ -1,4 +1,25 @@
 package poo.interfaces;
 
 public class Empleado {
+
+    // 1. Atributos
+    String nombre;
+    int edad;
+    double salario;
+    boolean alta;
+
+    // 2. cosntructores
+    public Empleado(){
+
+    }
+
+    public Empleado(String nombre, int edad, double salario, boolean alta) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.salario = salario;
+        this.alta = alta;
+    }
+
+
+    // 3. Metodos
 }
